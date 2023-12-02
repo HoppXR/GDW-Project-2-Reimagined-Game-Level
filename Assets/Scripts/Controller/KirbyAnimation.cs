acbdda10385e9f7e01b49e03e1c3f2d4624d2a3b
HoppXR <brtl.hopper@gmail.com>
2023-12-02 15:47:15 -0500

    Working jump animation

--- Assets/Scripts/Controller/KirbyAnimation.cs
+++ Assets/Scripts/Controller/KirbyAnimation.cs
@@ -4,33 +4,44 @@ using UnityEngine;
 
 public class KirbyAnimation : MonoBehaviour
 {
+    public LayerMask groundLayer;
+    
     public Animator animator;
-    private bool isGrounded;
 
     void Update()
-    {
-        CheckGrounded();
-
-        float horizontalMove = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
-        if (Input.GetButtonDown("Jump") && !isGrounded)
-        {
-            animator.SetBool("IsJumping", true);
-        }
-
-        if(isGrounded)
-        {
-            animator.SetBool("IsJumping", false);
-            Debug.Log("ON GROUND");
-        }
+    {   
+        jumpAni();
+        movingAni();
 
         bool isCrouch = Input.GetButton("Crouch");
         animator.SetBool("IsCrouching", isCrouch);
     }
 
-    void CheckGrounded()
+    private bool IsGrounded()
     {
-        
+        Vector2 raycastOrigin = transform.position - new Vector3(0f,1f,0f);
+
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.down, 0.2f, groundLayer);
+
+        return hit.collider != null;
+    }
+
+    void movingAni()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+    void jumpAni()
+    {
+        if (IsGrounded())
+        {
+            animator.SetBool("IsJumping", false);
+            Debug.Log("ON GROUND");
+        }
+        else if(!IsGrounded())
+        {
+            animator.SetBool("IsJumping", true);
+        }
     }
 }
