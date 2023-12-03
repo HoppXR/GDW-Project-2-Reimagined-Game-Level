37135d7141a9a05101d3cfa743d24bff0f96c4c2
HoppXR <brtl.hopper@gmail.com>
2023-12-02 23:27:13 -0500

    Star particle, Scene transitions

--- Assets/Scripts/SceneChangers/ChangeSceneOnTimer.cs
+++ Assets/Scripts/SceneChangers/ChangeSceneOnTimer.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeSceneOnTimer : MonoBehaviour
 {
-    public float changeTime;
-    public string sceneName;
+    public Animator transition;
 
+    public float changeTime;
+
+    private bool transitionStarted = false;
+
+    private void Start()
+    {
+        transition.SetTrigger("End");
+    }
+    
     private void Update()
     {
         changeTime -= Time.deltaTime;
-        if (changeTime <= 0)
+
+        if (changeTime <= 0 && !transitionStarted)
         {
-            SceneManager.LoadScene(sceneName);
+            transitionStarted = true;
+            LoadNextLevel();
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(ELoadLevel(SceneManager.GetActiveScene().buildIndex +1));
+    }
+
+    IEnumerator ELoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditorInternal;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
 
     public AudioSource buttonClickSound;
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("IntroScene");
         PlayButtonClickSound();
+
+        LoadNextLevel();
     }
 
     public void QuitGame()
@@ -29,11 +33,25 @@ public class MainMenu : MonoBehaviour
         #endif
     }
 
-        private void PlayButtonClickSound()
+    private void PlayButtonClickSound()
     {
         if (buttonClickSound != null)
         {
             buttonClickSound.Play();
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(ELoadLevel(SceneManager.GetActiveScene().buildIndex +1));
+    }
+
+    IEnumerator ELoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
